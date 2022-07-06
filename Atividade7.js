89887a23b11b6562b43dd49688256251b8abbc37
Arthur-Falcao <arthurmonteirofalcao1995@gmail.com>
2022-07-06 20:56:40 -0300

    Correções na estrutura de repetição

--- Atividade7.js
+++ Atividade7.js
@@ -10,84 +10,86 @@ ALGORITMO
   //Declarando Variáveis
   nome caractere;
   nota real;
-  contador, numAprovados1, numAprovados2, numAprovados3, numAprovados4, numTotalAprovados maiorNota1, maiorNota2, maiorNota3, maiorNota4, maiorNotaGeral, turma inteiro;
+  contador, numAprovados1, numAprovados2, numAprovados3, numAprovados4, numTotalAprovados maiorNota1, maiorNota2, maiorNota3, maiorNota4, maiorNotaGeral, turma, i inteiro;
   //Definindo valores iniciais
-  numAprovados1 = 0;
-  numAprovados2 = 0;
-  numAprovados3 = 0;
-  numAprovados4 = 0;
-  maiorNota1 = 0;
-  maiorNota2 = 0;
-  maiorNota3 = 0;
-  maiorNota4 = 0;
+  numAprovados1 <- 0;
+  numAprovados2 <- 0;
+  numAprovados3 <- 0;
+  numAprovados4 <- 0;
+  maiorNota1 <- 0;
+  maiorNota2 <- 0;
+  maiorNota3 <- 0;
+  maiorNota4 <- 0;
+  i <- 0;
 
 INICIO
-  escreva("De qual turma você é? Digite: 1 -TurmaA ou 2-TurmaB ou 3-TurmaC ou 4-TurmaD");
-  leia (turma)
-  SE (turma == 1) ENTAO
-    //Receber Dados da Turma A
-    PARA contador = 0; contador < 25 ; contador + 1
+  //Número total de alunos = 100
+  ENQUANTO ( i < 100 ) FACA
+    escreva("De qual turma você é? Digite: 1 -TurmaA ou 2-TurmaB ou 3-TurmaC ou 4-TurmaD");
+    leia (turma)
+    SE (turma = 1) ENTAO
+      //Receber Dados da Turma A
       escreva("Qual o seu nome?");
       leia (nome);
       escreva("Qual a sua nota?");
       leia (nota);
       //Para calcular o número de aprovados na turma A
-      SE (nota >= 7) ENTAO numAprovados1 = numAprovados1 + 1 FIMSE;
+      SE (nota >= 7) ENTAO numAprovados1 <- numAprovados1 + 1 FIMSE;
       //Para saber a maior nota da Turma A
-      SE (nota > maiorNota1) ENTAO maiorNota1 = nota FIMSE;
-    FIMPARA
+      SE (nota > maiorNota1) ENTAO maiorNota1 <- nota FIMSE;
+    
 
-  SENAO (turma == 2) ENTAO
-    //Receber Dados da Turma B
-    PARA contador = 0; contador < 25 ; contador + 1
+    SENAO (turma = 2) ENTAO
+      //Receber Dados da Turma B
       escreva("Qual o seu nome?");
       leia (nome);
       escreva("Qual a sua nota?");
       leia (nota);
       //Para calcular o número de aprovados na turma B
-      SE (nota >= 7) ENTAO numAprovados2 = numAprovados2 + 1 FIMSE;
+      SE (nota >= 7) ENTAO numAprovados2 <- numAprovados2 + 1 FIMSE;
       //Para saber a maior nota da Turma B
-      SE (nota > maiorNota2) ENTAO maiorNota2 = nota FIMSE;
-    FIMPARA
+      SE (nota > maiorNota2) ENTAO maiorNota2 <- nota FIMSE;
+      
 
-  SENAO (turma == 3) ENTAO
-    //Receber Dados da Turma C
-    PARA contador = 0; contador < 25 ; contador + 1
+    SENAO (turma = 3) ENTAO
+      //Receber Dados da Turma C
       escreva("Qual o seu nome?");
       leia (nome);
       escreva("Qual a sua nota?");
       leia (nota);
       //Para calcular o número de aprovados na turma C
-      SE (nota >= 7) ENTAO numAprovados3 = numAprovados3 + 1 FIMSE;
+      SE (nota >= 7) ENTAO numAprovados3 <- numAprovados3 + 1 FIMSE;
       //Para saber a maior nota da Turma C
-      SE (nota > maiorNota3) ENTAO maiorNota3 = nota FIMSE;
-    FIMPARA
-  
-  SENAO (turma == 4) ENTAO
-    //Receber Dados da Turma D
-    PARA contador = 0; contador < 25 ; contador + 1
+      SE (nota > maiorNota3) ENTAO maiorNota3 <- nota FIMSE;
+      
+    
+    SENAO (turma = 4) ENTAO
+      //Receber Dados da Turma D
       escreva("Qual o seu nome?");
       leia (nome);
       escreva("Qual a sua nota?");
       leia (nota);
       //Para calcular o número de aprovados na turma D
-      SE (nota >= 7) ENTAO numAprovados4 = numAprovados4 + 1 FIMSE;
+      SE (nota >= 7) ENTAO numAprovados4 <- numAprovados4 + 1 FIMSE;
       //Para saber a maior nota da Turma D
-      SE (nota > maiorNota4) ENTAO maiorNota4 = nota FIMSE;
-    FIMPARA
-  FIMSE
-  
+      SE (nota > maiorNota4) ENTAO maiorNota4 <- nota FIMSE;
+      
+    FIMSE
+
+    i <- i + 1;
+  FIMENQUANTO
+    
   //Cálculo do número total de aprovados
-  numTotalAprovados = numAprovados1 + numAprovados2 + numAprovados3 + numAprovados4;
+  numTotalAprovados <- numAprovados1 + numAprovados2 + numAprovados3 + numAprovados4;
 
   //Cálculo da maior nota de todas
   SE (maiorNota4 > maiorNota3) ENTAO
-    maiorNotaGeral = maiorNota4;
+    maiorNotaGeral <- maiorNota4;
   SENAO (maiorNota3 > maiorNota2)
-    maiorNotaGeral = maiorNota3;
+    maiorNotaGeral <- maiorNota3;
   SENAO (maiorNota2 > maiorNota1)
-    maiorNotaGeral = maiorNota2;
-  SENAO maiorNotaGeral = maiorNota1;
+    maiorNotaGeral <- maiorNota2;
+  SENAO maiorNotaGeral <- maiorNota1;
   FIMSE
 
   //Exibir Resultados:
