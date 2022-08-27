4f003d8ed75e0ab17269a5c4fbdd2e896e85c584
Arthur Monteiro Falcão <95629281+artfalcao@users.noreply.github.com>
2022-08-26 22:27:03 -0300

    Created GET request

--- Atividade-Metodos-HTTP/index.js
+++ Atividade-Metodos-HTTP/index.js
@@ -2,15 +2,20 @@ const express = require("express")
 const app = express()
 const port = 3000
 
+let users = [
+    {id: 0, nome: "Arthur", idade: 26}
+]
+
 app.get('/', (req, res) => {
-    res.send("Olá mundo!")
+    
+    res.send(users)
 })
 
+app.get('/:id', (req, res) => {
+    const id = req.params.id
 
-
-
-
-
+    res.status(200).send("Usuário: " + users[id].nome)
+})
 
 
 
