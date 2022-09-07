9a2e361c1e012607627e0b34cc07c422f3027658
Arthur Monteiro Falcão <95629281+artfalcao@users.noreply.github.com>
2022-09-07 10:04:38 -0300

    Add files via upload

---
+++ Atividade-Factory-Method/Computador.ts
@@ -0,0 +1,65 @@
+//Classe Abstrata Criadora - declara o método fábrica que deve retornar um objeto de uma classe produto
+abstract class FactoryComputador  {
+    //método abstrato
+    abstract criarComputador() : IComputador   
+}
+
+
+//Criadores Concretos - sobrescrevem o método fábrica para mudar o tipo de produto resultante.
+class ServerProduct extends FactoryComputador {
+    criarComputador(): IComputador {
+        return new Server
+    }
+}
+
+
+//Criadores Concretos - sobrescrevem o método fábrica para mudar o tipo de produto resultante.
+class PcProduct extends FactoryComputador {
+    criarComputador(): IComputador {
+        return new Pc
+    }
+}
+
+
+//Interface Produto - declara as operações que todos os produtos concretos devem implementar.
+interface IComputador {
+    ram: number
+    hd: number 
+    cpu: number 
+    type: string
+    toString() 
+}
+
+
+//Produtos concretos - fornecem implementações da interface do produto.
+class Server implements IComputador {
+    ram: number
+    hd: number 
+    cpu: number 
+    type: string
+
+    toString() {
+        return `
+            Ram: ${this.ram} GB; Hd: ${this.hd} GB; CPU: ${this.cpu} GHz; type: Server.
+        `
+    }
+
+}
+
+
+//Produtos concretos - fornecem implementações da interface do produto.
+class Pc implements IComputador {
+    ram: number
+    hd: number 
+    cpu: number 
+    type: string
+
+    toString() {
+        return `
+            Ram: ${this.ram} GB; Hd: ${this.hd} GB; CPU: ${this.cpu} GHz; type: Pc.
+        `
+    }
+}
+
+
+
