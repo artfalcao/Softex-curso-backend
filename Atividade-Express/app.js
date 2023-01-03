5a3ecdff70ee444e4ae536b956f06be75d809e74
Arthur Monteiro Falcão <95629281+artfalcao@users.noreply.github.com>
2023-01-03 11:25:49 -0300

    Update app.js

--- Atividade-Express/app.js
+++ Atividade-Express/app.js
@@ -1,1 +1,21 @@
-// Em construção
+const express = require('express')
+const app = express()
+  
+app.get(req, res) => {
+      
+  const responseData = {
+      message:"Hello!!",
+    articleData:{
+        articleName: "How to send JSON response from NodeJS",
+        category:"NodeJS",
+        status: "published"
+    }
+  }
+    
+  const jsonContent = JSON.stringify(responseData)
+  res.send(jsonContent)
+};
+    
+server.listen(3000, () => {
+    console.log("Server is Listening at Port 3000!")
+})
